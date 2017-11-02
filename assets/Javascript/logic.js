ad78b2871bcf9d7a9228847fc85ee3acf169be2e
unknown <yjeong19@gmail.com>
2017-11-02 09:58:21 -0400

    moved code to js page

--- assets/Javascript/logic.js
+++ assets/Javascript/logic.js
@@ -1,4 +1,27 @@
- 
+var database = firebase.database();
+var info = {
+  firstName: $("#fname").val(),
+  lastName: $("#lname").val(),
+  dob: $("#datepicker").val()
+}
+
+
+$("#submitBtn").on("click", function(){
+
+  database.ref("/User").push(
+    info = {
+      firstName: $("#fname").val(),
+      lastName: $("#lname").val(),
+      dob: $("#datepicker").val()
+
+  })
+
+
+});
+
+
+
+
   $( function() {
     $( "#datepicker" ).datepicker({
       showOtherMonths: true,
