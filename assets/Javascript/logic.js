a96c0da3e0a9088151bf332d6a1ea0001ec2c987
sophiasheref <sophia.sheref@gmail.com>
2017-11-02 14:31:26 -0400

    firebase

--- assets/Javascript/logic.js
+++ assets/Javascript/logic.js
@@ -7,33 +7,34 @@ var info = {
 }
 
 
-$("#submitBtn").on("click", function(){
-
-  database.ref("/User").push(
-    info = {
-      firstName: $("#fname").val(),
-      lastName: $("#lname").val(),
-      dob: $("#datepicker").val()
-
-  })
-
-
-});
-
+	
 //firebase end
 
 database.ref().child("/User").once("value", function(snapshot) {
   var userData = snapshot.val();
+  var keys = Object.keys(userData);
+  console.log(keys);
 
-  console.log(userData);
+for (var i = 0; i < keys.length; i++) {
+		var k = keys[i];
+		var first = userData[k].firstName;
+		var last = userData[k].lastName;
+		var bday = userData[k].dob;
+		console.log(first, last, bday);
+	}
 });
 
 
 
 
+
+
+  //datePicker function
   $( function() {
     $( "#datepicker" ).datepicker({
       showOtherMonths: true,
       selectOtherMonths: true
     });
   } );
+
+
