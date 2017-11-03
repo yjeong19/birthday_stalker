d4cb5584f62b4ea4a3ab70c1fc26836932089b10
unknown <yjeong19@gmail.com>
2017-11-02 23:28:11 -0400

    minor changes, made datepicker run only on click

--- assets/Javascript/logic.js
+++ assets/Javascript/logic.js
@@ -13,6 +13,10 @@ var bday;
 
 //firebase end
 
+$('.fc-next-button').on('click', function(){
+  alert('work');
+})
+
 $("#submitBtn").on("click", function(){
 
   database.ref("/User").push(
@@ -40,7 +44,9 @@ database.ref().child("/User").once("value", function(snapshot) {
       $(this).val(date);
       //the last was a test --- need to figure out how to make date input yyyy-mm-dd
       if("2017-"+bday === date){
-      $(this).text("It is " + first + "'s birthday! Click to send message")
+      $(this).text("It is " + first + "'s birthday! Click to send message");
+  } else if('2018-'+bday === date){
+      $(this).text("It is " + first + "'s birthday! Click to send message");
   }
 })
 }
@@ -50,6 +56,10 @@ database.ref().child("/User").once("value", function(snapshot) {
 
 
 
+//makes date only mm-dd for formatting
+$('#datepicker').on('click', function(){
+
 jQuery(function($){
    $("#datepicker").mask("99-99",{placeholder:"mm-dd"});
- });
+ })
+});
