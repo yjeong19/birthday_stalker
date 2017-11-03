e5f727fe98a6fff66c19162eb745e164c8301593
unknown <yjeong19@gmail.com>
2017-11-03 14:57:53 -0400

    includes for loop

--- assets/Javascript/logic.js
+++ assets/Javascript/logic.js
@@ -3,28 +3,32 @@ var database = firebase.database();
 var info = {
   firstName: $("#fname").val(),
   lastName: $("#lname").val(),
-  dob: $("#datepicker").val()
+  dob: $("#datepicker").val(),
+  check: false
 }
 var k;
 var first;
 var last;
 var bday;
+var date;
+
 
 
 //firebase end
 
-$('.fc-next-button').on('click', function(){
-  alert('work');
+$('.fc-next-button').on('click', function() {
+  console.log("clicked");
 })
 
-$("#submitBtn").on("click", function(){
+$("#submitBtn").on("click", function() {
 
   database.ref("/User").push(
     info = {
       firstName: $("#fname").val(),
       lastName: $("#lname").val(),
-      dob: $("#datepicker").val()
-  })
+      dob: $("#datepicker").val(),
+      check: false
+    })
 });
 
 database.ref().child("/User").once("value", function(snapshot) {
@@ -33,33 +37,68 @@ database.ref().child("/User").once("value", function(snapshot) {
   var keys = Object.keys(userData);
   // console.log(keys);
 
-  for (var i = 0; i < keys.length; i++){
+  for (var i = 0; i < keys.length; i++) {
     k = keys[i];
     first = userData[k].firstName;
     last = userData[k].lastName;
     bday = userData[k].dob;
+    check = userData[k].check;
+
+
 
     $('.fc-day').each(function() {
-      var date = $(this).data('date');
+      date = $(this).data('date');
       $(this).val(date);
       //the last was a test --- need to figure out how to make date input yyyy-mm-dd
-      if("2017-"+bday === date){
-      $(this).text("It is " + first + "'s birthday! Click to send message");
-  } else if('2018-'+bday === date){
-      $(this).text("It is " + first + "'s birthday! Click to send message");
+      if ("2017-" + bday === date) {
+        $(this).text("It is " + first + "'s birthday! Click to send message");
+        $(this).wrap('<a href="alert.html" target="_blank"/>');
+      } else if ('2018-' + bday === date) {
+        $(this).text("It is " + first + "'s birthday! Click to send message");
+        $(this).wrap('<a href="alert.html"/>');
+      }
+    })
   }
 })
-}
-});
+
 //firebase end
 
+//alert email
+$(function() {
+  $('#emailLink').on('click', function(event) {
+    database.ref().child('/User').once("value", function(snapshot) {
+      var userData = snapshot.val();
+      // console.log(userData);
+      var keys = Object.keys(userData);
+      // console.log(keys);
+
+      for (var i = 0; i < keys.length; i++) {
+        k = keys[i];
+        first = userData[k].firstName;
+        last = userData[k].lastName;
+        bday = userData[k].dob;
+
+        if (bday) {
+
+          // var email = last;
+          var subject = 'happy birthday';
+          var emailBody = 'Some blah';
+          window.location = 'mailto:' + last + '?subject=' + subject + '&body=' + emailBody;
+        }
+      }
+      console.log(userData[keys[1]].dob);
+    })
+  })
+})
 
 
 
 //makes date only mm-dd for formatting
-$('#datepicker').on('click', function(){
+$('#datepicker').on('click', function() {
 
-jQuery(function($){
-   $("#datepicker").mask("99-99",{placeholder:"mm-dd"});
- })
+  jQuery(function($) {
+    $("#datepicker").mask("99-99", {
+      placeholder: "mm-dd"
+    });
+  })
 });
